0126ea97263f881c1ffff3be63404181d507832a
Stephen Dowhy <sjd69@pitt.edu>
2018-03-22 16:05:40 -0700

    Rework of settings activity and fragment to include v7 improvements with recyclerView as the backing

--- app/src/main/java/sq/rogue/rosettadrone/SettingsActivity.java
+++ app/src/main/java/sq/rogue/rosettadrone/SettingsActivity.java
@@ -1,18 +1,27 @@
 package sq.rogue.rosettadrone;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
 
-        // Display the fragment as the main content.
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
+        if (savedInstanceState == null) {
+            Fragment preferenceFragment = new SettingsFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.pref_container, preferenceFragment);
+            fragmentTransaction.commit();
+        }
+//        // Display the fragment as the main content.
+//        getFragmentManager().beginTransaction()
+//                .replace(android.R.id.content, new SettingsFragment())
+//                .commit();
     }
 
 }
--- app/src/main/java/sq/rogue/rosettadrone/SettingsFragment.java
+++ app/src/main/java/sq/rogue/rosettadrone/SettingsFragment.java
@@ -2,71 +2,78 @@ package sq.rogue.rosettadrone;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.EditTextPreference;
-import android.preference.ListPreference;
-import android.preference.MultiSelectListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceScreen;
+import android.support.v14.preference.MultiSelectListPreference;
+import android.support.v7.preference.EditTextPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceGroup;
 
 // Display value of preference in summary field
 
-import java.util.Arrays;
-
-public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
     SharedPreferences sharedPreferences;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        sharedPreferences = getPreferenceManager().getSharedPreferences();
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
 
-        // Load the preferences from an XML resource
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preferences);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        sharedPreferences = getPreferenceManager().getSharedPreferences();
-        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
 
-        PreferenceScreen preferenceScreen = getPreferenceScreen();
-        for (int i = 0; i < preferenceScreen.getPreferenceCount(); i++) {
-            setSummary(getPreferenceScreen().getPreference(i));
+        for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); ++i) {
+            Preference preference = getPreferenceScreen().getPreference(i);
+            if (preference instanceof PreferenceGroup) {
+                PreferenceGroup preferenceGroup = (PreferenceGroup) preference;
+                for (int j = 0; j < preferenceGroup.getPreferenceCount(); ++j) {
+                    Preference singlePref = preferenceGroup.getPreference(j);
+                    updatePreference(singlePref);
+                }
+            } else {
+                updatePreference(preference);
+            }
         }
     }
 
     @Override
     public void onPause() {
-        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
         super.onPause();
     }
 
     @Override
+    public void onDestroy() {
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+        super.onDestroy();
+    }
+    @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        Preference pref = getPreferenceScreen().findPreference(key);
-        setSummary(pref);
+        updatePreference(findPreference(key));
     }
 
-    private void setSummary(Preference pref) {
-        if (pref instanceof EditTextPreference) {
-            updateSummary((EditTextPreference) pref);
-        } else if (pref instanceof ListPreference) {
-            updateSummary((ListPreference) pref);
-        } else if (pref instanceof MultiSelectListPreference) {
-            updateSummary((MultiSelectListPreference) pref);
+    private void updatePreference(Preference preference) {
+        if (preference == null) return;
+        if (preference instanceof EditTextPreference) {
+            EditTextPreference editTextPref = (EditTextPreference) preference;
+            preference.setSummary(editTextPref.getText());
+        } else if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            listPreference.setSummary(listPreference.getEntry());
+            return;
+        } else {
+            return;
         }
-    }
-
-    private void updateSummary(MultiSelectListPreference pref) {
-        pref.setSummary(Arrays.toString(pref.getValues().toArray()));
-    }
-
-    private void updateSummary(ListPreference pref) {
-        pref.setSummary(pref.getValue());
-    }
-
-    private void updateSummary(EditTextPreference preference) {
-        preference.setSummary(preference.getText());
+        SharedPreferences sharedPrefs = getPreferenceManager().getSharedPreferences();
+        preference.setSummary(sharedPrefs.getString(preference.getKey(), "Default"));
     }
 }
