bcfccd7867a2749acff37836ed5261c72fc5a3f3
Yuwooooh <YW13927987651@163.com>
2021-12-29 13:40:48 +0800

    opencv test

--- app/src/main/java/sq/rogue/rosettadrone/fragment/actuator/IActuatorCallback.java
+++
@@ -1,7 +0,0 @@
-package sq.rogue.rosettadrone.fragment.actuator;
-
-import dji.common.mission.waypointv2.Action.WaypointActuator;
-
-public interface IActuatorCallback {
-    WaypointActuator getActuator();
-}
